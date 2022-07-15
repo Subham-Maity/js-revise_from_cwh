585769afc4d9ad6b703f24c4a1faf449b1818c05
Subham Maity <97989643+Subham-Maity@users.noreply.github.com>
2022-07-16 01:52:59 +0530

    Day 2 code add - variable intro

---
+++ 002. Variables in JavaScript/var.js
@@ -0,0 +1,7 @@
+console.log("This repl contains code for the Ultimate JavaScript course video no 2")
+var a = 67 // a contains 67
+console.log(a) // prints 67
+a = "Subham" // a contains "Subham"
+console.log(a)
+// let 8subh = 7 // Not allowed this will throw an error
+// let var = 7
